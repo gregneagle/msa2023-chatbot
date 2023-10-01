55c5a0f2c31ecb2eab769cdd81755980870b4c7c
Greg Neagle <gregneagle@mac.com>
2023-09-30 19:39:20 -0700

    Remove quarantine attribute before running Python

--- run_local_chatbot.sh
+++ run_local_chatbot.sh
@@ -4,4 +4,5 @@ SOURCE=${BASH_SOURCE[0]}
 THISDIR=$(dirname "${SOURCE}")
 PYTHON="${THISDIR}/Python.framework/Versions/Current/bin/python3"
 cd "$THISDIR"
+xattr -dr com.apple.quarantine Python.framework
 ${PYTHON} ./local_chatbot.py
