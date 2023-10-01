1c984e149081f4749c90757e40b9db0bdb4b1fd7
Greg Neagle <gregneagle@mac.com>
2023-09-30 19:49:13 -0700

    Verify documents folder is populated

--- run_local_chatbot.sh
+++ run_local_chatbot.sh
@@ -5,4 +5,8 @@ THISDIR=$(dirname "${SOURCE}")
 PYTHON="${THISDIR}/Python.framework/Versions/Current/bin/python3"
 cd "$THISDIR"
 xattr -dr com.apple.quarantine Python.framework
+if [ -e "documents/README.txt" ] ;  then
+    echo "****** Did you copy documents into the documents folder, and did you remove the default README.txt? ********"
+    sleep 2
+fi
 ${PYTHON} ./local_chatbot.py
